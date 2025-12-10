b309d70193c11105b6515b140fa4ed33d5ef0539
Monugya | curious_coder <monugyaborchetia1@gmail.com>
2025-12-10 12:05:00 +0000

    Commit

--- 05_python/04.py
+++ 05_python/04.py
@@ -8,3 +8,30 @@ def initialize_mat(dim):
         for j in range(dim):
             c[i].append(0) 
     return c 
+
+def dot_product(u,v):
+    dim = len(u)
+    ans = 0
+    for i in range(dim):
+        ans = ans+(u[i]*v[i])
+    return ans 
+
+def row(M,i):
+    dim = len(M)
+    l=[]
+    for k in range(dim):
+        l.append(M[i][k])
+    return l 
+
+def column(M,j):
+    dim = len(M)
+    l = []
+    for k in range(dim):
+        l.append(M[k][j]) 
+    return l 
+
+def mat_mul(A,B):
+    dim=len(A)
+    C = initialize_mat(dim)
+    # for i in range(dim):
+    #     for j in range(dim):
