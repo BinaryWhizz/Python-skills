43e9875b053c9f04dd08e278a9b0f05a808a8e3b
Monugya | curious_coder <monugyaborchetia1@gmail.com>
2025-12-12 20:53:00 +0000

    Loop questions

--- 03_python/07_PPA.py
+++ 03_python/07_PPA.py
@@ -1,3 +1,41 @@
+# Count the positive numbers in a list - [1,-2,3,-4,5,6,-7,-8,9,10]
+
+# n = [1,-2,3,-4,5,6,-7,-8,9,10]
+
+# c = 0
+
+# for i in n:
+#     if i>0:
+#         c+=1
+# print(c)
+    
+
+# Sum of even numbers - Calculate the sum of even numbers up to a given number n.
+
+# n = int(input())
+
+# c = 0
+
+# for i in range(1,n+1):
+#     if i%2 == 0 :
+#         c += i
+# print(c)
+
+
+# Multiplication table printer - Print the multiplication table for a given number up to 10 
+
+# n = int(input())
+
+# for i in range(1,n+1):
+#     print(n,"x",i,'=',n*i)
+
+# Find the first non-repeated character
+
+
+
+
+
+
 # PPA 1
 # Accept a positive integer n as input and print the first n positive
 # integers, one number on each line.
