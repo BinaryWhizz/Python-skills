3a2ad06fae219b99c6f5af0843e82c91f8590007
Monugya | curious_coder <monugyaborchetia1@gmail.com>
2025-12-16 16:35:54 +0000

    more on lists

---
+++ 05_python/08.py
@@ -0,0 +1,71 @@
+# More on Lists 
+
+l1 = [1,2,3]
+l2 = [4,5,6]
+
+l12 = l1 + l2
+l21 = l2 + l1
+
+print(l12, l21)         # [1, 2, 3, 4, 5, 6] [4, 5, 6, 1, 2, 3] 
+
+
+# l3 = [0,0,0,0,0,0,0,0,0,0] 
+
+l3 = [0] * 10 
+print(l3)
+
+print(l1 * 5)           # [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3] 
+
+
+l1 = [1,2,3]
+l2 = [1,2,3]
+l3 = [1,3,2] 
+
+print(l1 == l2)         # True  
+print(l2 == l3)         # False   
+print(l2 > l3)          # False  
+
+print([1,2] < [2,1])    # True 
+print([1] < [1,2,3])    # True 
+print([2,3] < [3])      # True 
+print([] < [1])         # True 
+
+
+l = [1,2,4]
+print(l)                # [1, 2, 4]
+l[2] = 3
+print(l)                # [1, 2, 3] 
+
+
+
+# It cannot change value of a specific character 
+
+# s = 'abcde'
+# print(s[3]) 
+# s[3] = 'd'
+# print(s)            # Error 
+
+
+x = 5
+y = x
+x = 10
+print(x,y)
+
+
+l1 = [1,2,3]
+l2 = l1
+l1[0] = 100 
+print(l1, l2)         # [100, 2, 3] [100, 2, 3] , reason : Internal impementaion of integers and lists in python language 
+
+
+
+l4 = [1,2,3]
+l5 = list(l4)
+l6 = l1[:]
+l7 = l1.copy() 
+
+l5[0] = 100
+l6[1] = 200
+l7[2] = 300 
+
+print(l1, l2, l3, l4)
