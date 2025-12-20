c8e6548c05c5f53017d3b6cc602f86fcfd08f52e
Monugya | curious_coder <monugyaborchetia1@gmail.com>
2025-12-20 17:12:47 +0530

    Types of functions

---
+++ 06_python/06.py
@@ -0,0 +1,29 @@
+# Types of functions 
+
+
+# first category function ---> Inbuilt functions
+ 
+# 'print()', 'input()', 'len()'                            
+
+
+# Note : parenthesis is the standard way of representing a function, every function is always followed by a parenthesis
+
+
+# 2nd category function ----> Library functions 
+  
+# log(), sqrt(), random(), randrange(), calender(), month() 
+
+
+# 3rd category function ----> String methods ( functions )
+
+# upper(), lower(), strip(), count(), index(), replace() 
+
+
+# 4th category function ---> User defined functions 
+
+# def square(x):
+#     sqr = x ** 2
+#     return sqr
+
+# print(square(5)) 
+
