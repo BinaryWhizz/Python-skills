660a4a37c720a75cf415710dc56dca98decc2ff1
Monugya | curious_coder <monugyaborchetia1@gmail.com>
2025-12-28 16:38:26 +0000

    Pandas & NumPy

---
+++ 09_python/03.py
@@ -0,0 +1,56 @@
+
+# Pandas : Pandas is a Python library used for data manipulation and analysis.
+
+# Key points:
+
+# It provides data structures like Series (1D) and DataFrame (2D tables).
+# Commonly used to read, clean, transform, analyze, and export data.
+# Supports files such as CSV, Excel, SQL, JSON, etc.
+# Widely used in data analysis, data science, and web scraping. 
+
+# Example:
+
+# import pandas as pd
+
+# data = {
+#     "Name": ["Alex", "Akash"],
+#     "Age": [20, 22]
+# }
+
+# df = pd.DataFrame(data)
+# print(df) 
+
+
+
+# NumPy (Numerical Python) is a Python library used for numerical computing and mathematical operations.
+
+# Key points:
+
+# Provides support for multi-dimensional arrays and matrices.
+# Much faster than Python lists for numerical calculations.
+# Includes functions for mathematics, statistics, linear algebra, and more.
+# Widely used in data science, machine learning, and scientific computing.
+
+# Example:
+
+# import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5])
+# print(arr)
+# print(arr * 2) 
+
+
+
+
+# In terminal -
+
+# pip install pandas
+# pip install numpy 
+
+# Now -
+
+# import pandas as pd
+# import numpy as np
+
+# data = pd.read_excel('customer_data.xlsx')
+# data.head()
