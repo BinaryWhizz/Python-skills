75a94fabb068a390519de220974a8d27a32e4f14
Monugya | curious_coder <monugyaborchetia1@gmail.com>
2026-03-31 21:34:21 +0530

    Implement dictionary value combination program
    Added a program to combine two dictionaries by summing values for common keys.

--- 05_python/10.py
+++ 05_python/10.py
@@ -22,6 +22,24 @@ for i in d:
 
 d = {0:0, 1:1, 2:4, 3:9, 4:16}
 
+
+
+
+
+# Write a Python program to combine two dictionary by adding
+# values for common keys.
+
+d1 = {10:100, 20:200, 40:300}
+d2 = {40:400, 50:500, 60:600}
+
+for i in d2:
+    if i in d1.keys():
+        d1[i] += d2[i]
+    else:
+        d1[i] = d2[i]
+
+print(d1)
+
 print(d.keys())                  # dict_keys([0, 1, 2, 3, 4])         , in a list
 print(d.values())                # dict_values([0, 1, 4, 9, 16])      , in a list 
 print(d.items())                 # dict_items([(0, 0), (1, 1), (2, 4), (3, 9), (4, 16)]), This is another place where python internally use tuples
